696a25e04cb3b4a274fb4e7437e149a8c251e49b
BlueHorn07 <bluehornblues@gmail.com>
2021-04-02 19:39:30 +0900

    sqlite3 setting done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import { InpostackModule } from './inpostack/inpostack.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    InpostackModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
