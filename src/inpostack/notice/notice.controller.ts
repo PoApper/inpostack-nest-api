60dc6a64170417cb42b0467775d60ebc41ef93ce
jjeongone <jjeongone@gmail.com>
2021-05-30 17:52:13 +0900

    add swagger component to notice

--- src/inpostack/notice/notice.controller.ts
+++ src/inpostack/notice/notice.controller.ts
@@ -1,7 +1,7 @@
 import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
 import {NoticeService} from "./notice.service";
 import {NoticeDto} from "./notice.dto";
-import { ApiTags } from "@nestjs/swagger";
+import {ApiOperation, ApiTags} from "@nestjs/swagger";
 
 @ApiTags('Notice')
 @Controller('notice')
@@ -10,26 +10,31 @@ export class NoticeController {
     }
 
     @Post()
+    @ApiOperation({summary: 'create notice API', description: 'create the new notice'})
     post(@Body() dto: NoticeDto) {
         return this.noticeService.save(dto);
     }
 
     @Get()
+    @ApiOperation({summary: 'get all notice API', description: 'get whole notices'})
     get() {
         return this.noticeService.find({order: {createAt: "DESC"}});
     }
 
     @Get("uuid")
+    @ApiOperation({summary: 'get specific notice API', description: 'get the specific notice using uuid'})
     getOne(@Param("uuid") uuid: string) {
         return this.noticeService.findOne({uuid: uuid});
     }
 
     @Put("uuid")
+    @ApiOperation({summary: 'update notice API', description: 'update the specific notice using uuid'})
     put(@Param("uuid") uuid: string, @Body() dto: NoticeDto) {
         return this.noticeService.update({uuid: uuid}, dto);
     }
 
     @Delete("uuid")
+    @ApiOperation({summary: 'delete notice API', description: 'delete the specific notice using uuid'})
     delete(@Param("uuid") uuid: string) {
         return this.noticeService.delete({uuid: uuid});
     }
--- src/inpostack/notice/notice.dto.ts
+++ src/inpostack/notice/notice.dto.ts
@@ -1,7 +1,11 @@
 import {NoticeType} from "./notice.meta";
+import {ApiProperty} from "@nestjs/swagger";
 
 export class NoticeDto{
+    @ApiProperty({type: String, description: 'notice title'})
     readonly title: string;
+    @ApiProperty({type: String, description: 'notice body content'})
     readonly content: string;
+    @ApiProperty({type: String, description: 'notice type(GLOBAL/STORE-OWNER/USER)'})
     readonly noticeType: NoticeType;
 }
