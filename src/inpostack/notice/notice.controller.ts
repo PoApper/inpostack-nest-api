04d83cc1c19a16a81bbe7d3ff20dae004ab01db2
jjeongone <jjeongone@gmail.com>
2021-05-30 20:41:11 +0900

    fix type and some logic

--- src/inpostack/notice/notice.controller.ts
+++ src/inpostack/notice/notice.controller.ts
@@ -10,7 +10,7 @@ export class NoticeController {
     }
 
     @Post()
-    @ApiOperation({summary: 'create notice API', description: 'create the new notice'})
+    @ApiOperation({summary: 'create notice API', description: 'create a new notice'})
     post(@Body() dto: NoticeDto) {
         return this.noticeService.save(dto);
     }
@@ -21,20 +21,20 @@ export class NoticeController {
         return this.noticeService.find({order: {createAt: "DESC"}});
     }
 
-    @Get("uuid")
-    @ApiOperation({summary: 'get specific notice API', description: 'get the specific notice using uuid'})
+    @Get(":uuid")
+    @ApiOperation({summary: 'get specific notice API', description: 'get a specific notice using uuid'})
     getOne(@Param("uuid") uuid: string) {
         return this.noticeService.findOne({uuid: uuid});
     }
 
-    @Put("uuid")
-    @ApiOperation({summary: 'update notice API', description: 'update the specific notice using uuid'})
+    @Put(":uuid")
+    @ApiOperation({summary: 'update notice API', description: 'update a specific notice using uuid'})
     put(@Param("uuid") uuid: string, @Body() dto: NoticeDto) {
         return this.noticeService.update({uuid: uuid}, dto);
     }
 
-    @Delete("uuid")
-    @ApiOperation({summary: 'delete notice API', description: 'delete the specific notice using uuid'})
+    @Delete(":uuid")
+    @ApiOperation({summary: 'delete notice API', description: 'delete a specific notice using uuid'})
     delete(@Param("uuid") uuid: string) {
         return this.noticeService.delete({uuid: uuid});
     }
