de8fb37ed1e97b45372d90437719828ce72e4528
bluehorn07 <bluehornblues@gmail.com>
2021-07-24 15:48:26 +0900

    [api] add auth/me, and auth/me/store

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,34 @@
-import { Body, Controller, Get, Inject, Param, Post, Query, Req, Res, UseGuards } from '@nestjs/common';
-import { AuthGuard } from "@nestjs/passport";
-import { AuthService } from "./auth.service";
-import { Request, Response } from "express";
-import { AccountService } from "../inpostack/account/account.service";
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Post,
+  Query,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { Request, Response } from 'express';
+import { ApiOperation } from '@nestjs/swagger';
+import { MailService } from '../mail/mail.service';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
+import { AccountService } from '../inpostack/account/account.service';
 import { AccountStatus, AccountType } from '../inpostack/account/account.meta';
-import { AccountCreateDto } from "../inpostack/account/account.dto";
-import { ApiOperation } from "@nestjs/swagger";
-import { MailService } from "../mail/mail.service";
-import { WINSTON_MODULE_PROVIDER } from "nest-winston";
-import { Logger } from "winston";
+import { AccountCreateDto } from '../inpostack/account/account.dto';
 import { StoreService } from '../inpostack/store/store.service';
+import { AccountTypeGuard } from './role.guard';
+import { AccountTypes } from './role.decorator';
 
 /**
  * This is for handle "verifyToken", "login", "logout" tasks
  */
 
-@Controller("auth")
+@Controller('auth')
 export class AuthController {
   constructor(
     private authService: AuthService,
@@ -23,17 +36,17 @@ export class AuthController {
     private readonly storeService: StoreService,
     private readonly mailService: MailService,
     @Inject(WINSTON_MODULE_PROVIDER)
-    private readonly logger: Logger
-  ) {
-  }
+    private readonly logger: Logger,
+  ) {}
 
-  @Get("verifyToken")
-  @UseGuards(AuthGuard("jwt"))
+  // TODO: will be deprecated
+  @Get('verifyToken')
+  @UseGuards(AuthGuard('jwt'))
   async verifyToken(@Req() req: Request) {
     const user: any = req.user;
 
-    if(user.account_type == AccountType.storeOwner) {
-      user.store = await this.storeService.findOne({owner_uuid: user.uuid});
+    if (user.account_type == AccountType.storeOwner) {
+      user.store = await this.storeService.findOne({ owner_uuid: user.uuid });
     }
 
     console.log(user);
@@ -41,52 +54,82 @@ export class AuthController {
     return user;
   }
 
-  @Post("login")
-  @UseGuards(AuthGuard("local"))
+  /**
+   * Return account information
+   */
+  @Get('me')
+  @UseGuards(AuthGuard('jwt'))
+  async getAccountInfo(@Req() req: Request) {
+    const user: any = req.user;
+    return user;
+  }
+
+  /**
+   * Return store information
+   */
+  @Get('me/store')
+  @UseGuards(AuthGuard('jwt'), AccountTypeGuard)
+  @AccountTypes(AccountType.storeOwner)
+  etStoreInfo(@Req() req: Request) {
+    const user: any = req.user;
+    return this.storeService.findOne({ owner_uuid: user.uuid });
+  }
+
+  @Post('login')
+  @UseGuards(AuthGuard('local'))
   async login(@Req() req: Request, @Res() res: Response) {
     try {
       const user: any = req.user;
       const token = this.authService.generateJwtToken(user);
-      res.setHeader(
-        "Set-Cookie", `Authentication=${token}; HttpOnly; Path=/;`
-      );
+      res.setHeader('Set-Cookie', `Authentication=${token}; HttpOnly; Path=/;`);
       this.accountService.updateLoginById(user.id);
-      this.logger.info(`Login Succeed!: uuid=${user.uuid}`)
+      this.logger.info(`Login Succeed!: uuid=${user.uuid}`);
       return res.send(user);
-    } catch(err) {
+    } catch (err) {
       this.logger.error(`Failed to login...${err}`);
     }
   }
 
-  @Get("logout")
-  @UseGuards(AuthGuard("jwt"))
+  @Get('logout')
+  @UseGuards(AuthGuard('jwt'))
   logout(@Res() res: Response) {
     try {
-      res.setHeader("Set-Cookie",
-        `Authentication=; HttpOnly; Path=/; Max-Age=0`);
+      res.setHeader(
+        'Set-Cookie',
+        `Authentication=; HttpOnly; Path=/; Max-Age=0`,
+      );
       this.logger.info(`Logout Succeed!`);
       return res.sendStatus(200);
-    } catch(err) {
+    } catch (err) {
       this.logger.error(`Failed to logout...${err}`);
     }
   }
 
-  @Post("register")
-  @ApiOperation({ summary: "회원가입", description: "계정 생성 & 인증 메일 발송" })
-  async register(@Body() dto: AccountCreateDto, @Query("sendMail") sendMail?: boolean) {
+  @Post('register')
+  @ApiOperation({
+    summary: '회원가입',
+    description: '계정 생성 & 인증 메일 발송',
+  })
+  async register(
+    @Body() dto: AccountCreateDto,
+    @Query('sendMail') sendMail?: boolean,
+  ) {
     let newAccount;
     try {
       newAccount = await this.accountService.save(dto);
       console.log(newAccount);
-    } catch(err) {
+    } catch (err) {
       this.logger.error(`Failed to register...${err}`);
     }
 
     if (sendMail) {
       try {
-        this.mailService.sendVerificationMail(newAccount.email, newAccount.uuid);
-        this.logger.info("Succeed to send email!");
-      } catch(err) {
+        this.mailService.sendVerificationMail(
+          newAccount.email,
+          newAccount.uuid,
+        );
+        this.logger.info('Succeed to send email!');
+      } catch (err) {
         this.logger.error(`Failed to send email...${err}`);
       }
     }
@@ -94,14 +137,17 @@ export class AuthController {
     return newAccount;
   }
 
-  @Get("activateAccount/:uuid")
-  async activateAccount(@Param("uuid") uuid: string, @Res() res: Response) {
+  @Get('activateAccount/:uuid')
+  async activateAccount(@Param('uuid') uuid: string, @Res() res: Response) {
     try {
       await this.accountService.findOneOrFail({ uuid: uuid });
-      await this.accountService.update({ uuid: uuid }, { account_status: AccountStatus.activated });
+      await this.accountService.update(
+        { uuid: uuid },
+        { account_status: AccountStatus.activated },
+      );
       this.logger.info(`Succeed to active account: uuid=${uuid}`);
       res.redirect(process.env.public_web);
-    } catch(err) {
+    } catch (err) {
       this.logger.error(`Failed to activate account...${err}`);
     }
   }
