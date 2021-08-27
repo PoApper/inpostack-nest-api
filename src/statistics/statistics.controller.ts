057a16b556ff77d42abeb8f76f83e8355deb4f98
jjeongone <jjeongone@gmail.com>
2021-08-27 23:34:15 +0900

    [fix] remove unnecessary relation and edit query

--- src/statistics/statistics.controller.ts
+++ src/statistics/statistics.controller.ts
@@ -1,14 +1,10 @@
 import { Controller, Get, Query } from "@nestjs/common";
-import { AccountService } from "../inpostack/account/account.service";
 import { createQueryBuilder } from "typeorm";
 import { ApiOperation, ApiTags } from "@nestjs/swagger";
 
 @ApiTags("Statistics")
 @Controller("statistics")
 export class StatisticsController {
-  constructor(private readonly userService: AccountService) {
-  }
-
   @ApiOperation({ summary: "Daily Register User" })
   @Get("DRU")
   async dailyRegisterUser(
@@ -19,7 +15,6 @@ export class StatisticsController {
       .select("created_at")
       .addSelect("COUNT(uuid) AS DRU")
       .where(`created_at BETWEEN '${start_date}' AND '${end_date}'`)
-      .groupBy("created_at")
       .orderBy("created_at")
       .getRawMany();
   }
@@ -31,7 +26,7 @@ export class StatisticsController {
     @Query("end_date") end_date: Date
   ) {
     return createQueryBuilder("account")
-      .select("DATE_FORMAT(last_login_at, %Y%m%d) AS login_at")
+      .select("DATE(last_login_at) AS login_at")
       .addSelect("COUNT(uuid) AS DAU")
       .where(`last_login_at BETWEEN '${start_date}' AND '${end_date}'`)
       .orderBy("login_at")
@@ -44,7 +39,7 @@ export class StatisticsController {
     @Query("start_date") start_date: Date,
     @Query("end_date") end_date: Date
   ) {
-    return createQueryBuilder("storevisit")
+    return createQueryBuilder("store_visit")
       .select("visited_at")
       .addSelect("COUNT(uuid) AS DVU")
       .where(`visited_at BETWEEN '${start_date}' AND '${end_date}'`)
