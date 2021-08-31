a8ad7a5d4eae7c4e751f27d14f5dd726147fa31a
bluehorn07 <bluehornblues@gmail.com>
2021-08-31 23:51:11 +0900

    [dev] implt router for easy db set-up

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,10 +6,34 @@ import { AccountType } from './inpostack/account/account.meta';
 import { StoreGuard } from './auth/guard/store.guard';
 import { JwtGuard } from './auth/guard/jwt.guard';
 import { AllowAnonymous } from './auth/decorator/anonymous.decorator';
+import * as fs from 'fs';
+import { StoreType } from './inpostack/market/store/store.meta';
+import { CsvParser } from 'nest-csv-parser';
+import { StoreService } from './inpostack/market/store/store.service';
+import { CategoryService } from './inpostack/market/category/category.service';
+import { MenuService } from './inpostack/market/menu/menu.service';
+import { LoremIpsum } from 'lorem-ipsum';
+
+class StoreDummy {
+  name: string;
+  store_type: StoreType;
+}
+
+class CategoryMenuDummy {
+  store_name: string;
+  category_name: string;
+  menu_name: string;
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly storeService: StoreService,
+    private readonly categoryService: CategoryService,
+    private readonly menuService: MenuService,
+    private readonly csvParser: CsvParser,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -41,5 +65,102 @@ export class AppController {
   @AccountTypes(AccountType.storeOwner)
   testStoreAuth(@Req() req) {
     return req.user;
+  }
+
+  @Get('default_store_set_up')
+  async defaultStoreSetUp() {
+    console.log(__dirname);
+    const stream = fs.createReadStream('./default_stores.csv');
+    const result = await this.csvParser.parse(stream, StoreDummy, null, null, {
+      strict: true,
+      separator: ',',
+    });
+    const storeDummies: StoreDummy[] = result['list'];
+
+    const lorem = new LoremIpsum({
+      sentencesPerParagraph: {
+        max: 8,
+        min: 4,
+      },
+      wordsPerSentence: {
+        max: 16,
+        min: 4,
+      },
+    });
+
+    for (const store_dummy of storeDummies) {
+      this.storeService.save({
+        name: store_dummy.name,
+        phone: '010-0000-0000',
+        description: lorem.generateSentences(4),
+        store_type: store_dummy.store_type,
+        address1: '',
+        address2: '',
+        zipcode: 0,
+        open_time: '0900',
+        close_time: '2100',
+      });
+    }
+
+    return storeDummies;
+  }
+
+  @Get('default_category_menu_set_up')
+  async defaultCategoryMenuSetUp() {
+    const stream = fs.createReadStream('./default_category_and_menu.csv');
+    const result = await this.csvParser.parse(
+      stream,
+      CategoryMenuDummy,
+      null,
+      null,
+      { strict: true, separator: ',' },
+    );
+    const categoryMenuDummies: CategoryMenuDummy[] = result['list'];
+
+    const lorem = new LoremIpsum({
+      sentencesPerParagraph: {
+        max: 8,
+        min: 4,
+      },
+      wordsPerSentence: {
+        max: 16,
+        min: 4,
+      },
+    });
+
+    for (const categoryMenuDummy of categoryMenuDummies) {
+      const store = await this.storeService.findOne({
+        name: categoryMenuDummy.store_name,
+      });
+
+      if (!store) {
+        console.log(categoryMenuDummy);
+        continue;
+      }
+
+      let category = await this.categoryService.findOne({
+        name: categoryMenuDummy.category_name,
+      });
+
+      if (!category) {
+        // create new category
+        category = await this.categoryService.save({
+          name: categoryMenuDummy.category_name,
+          description: lorem.generateSentences(1),
+          store_uuid: store.uuid,
+        });
+      }
+
+      this.menuService.save({
+        category_uuid: category.uuid,
+        store_uuid: store.uuid,
+        name: categoryMenuDummy.menu_name,
+        price: (Math.floor(Math.random() * 100) + 1) * 1000,
+        like: Math.floor(Math.random() * 100),
+        hate: Math.floor(Math.random() * 100),
+        description: lorem.generateSentences(3),
+      });
+    }
+    return categoryMenuDummies;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,9 @@ import { LoggerModule } from './logger/logger.module';
 import { AccountModule } from './inpostack/account/account.module';
 import { StoreModule } from './inpostack/market/store/store.module';
 import { StatisticsModule } from './statistics/statistics.module';
+import { CsvModule } from 'nest-csv-parser';
+import { MenuModule } from './inpostack/market/menu/menu.module';
+import { CategoryModule } from './inpostack/market/category/category.module';
 
 @Module({
   imports: [
@@ -18,10 +21,13 @@ import { StatisticsModule } from './statistics/statistics.module';
     InpostackModule,
     AccountModule,
     StoreModule,
+    CategoryModule,
+    MenuModule,
     AuthModule,
     MailModule,
     LoggerModule,
     StatisticsModule,
+    CsvModule,
   ],
   controllers: [AppController],
   providers: [AppService],
