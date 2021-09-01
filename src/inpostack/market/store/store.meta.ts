619459a3cb56613a0d40f57d75c7d7f0da190cbc
bluehorn07 <bluehornblues@gmail.com>
2021-09-01 18:50:01 +0900

    [meta] update store types

--- src/inpostack/market/store/store.meta.ts
+++ src/inpostack/market/store/store.meta.ts
@@ -8,5 +8,10 @@ export enum StoreType {
   japanese = 'JAPANESE',
   chinese = 'CHINESE',
   instant = 'INSTANT',
+  chicken = 'CHICKEN',
+  hamburger = 'HAMBURGER',
+  pizza = 'PIZZA',
+  snack_shop = 'SNACK_SHOP', // 분식
+  sea_food = 'SEA_FOOD',
   etc = 'ETC',
 }
