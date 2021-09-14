9040a6c84a91c4833d1ede9a7a77816d11f1b9c6
jjeongone <jjeongone@gmail.com>
2021-09-14 22:41:24 +0900

    implement review service and controller

--- src/inpostack/market/review/review.service.ts
+++ src/inpostack/market/review/review.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Review } from './review.entity';
 import { Repository } from 'typeorm';
+import { ReviewDto, ReviewUpdateDto } from './review.dto';
 
 @Injectable()
 export class ReviewService {
@@ -9,4 +10,28 @@ export class ReviewService {
     @InjectRepository(Review)
     private readonly reviewRepo: Repository<Review>,
   ) {}
+
+  save(dto: ReviewDto) {
+    return this.reviewRepo.save(dto);
+  }
+
+  find(findOptions?: object) {
+    return this.reviewRepo.find(findOptions);
+  }
+
+  findOne(findOptions: object, maybeOptions?: object) {
+    return this.reviewRepo.findOne(findOptions, maybeOptions);
+  }
+
+  findOneOrFail(findOptions: object) {
+    return this.reviewRepo.findOneOrFail(findOptions);
+  }
+
+  update(findOptions: object, dto: ReviewUpdateDto) {
+    return this.reviewRepo.update(findOptions, dto);
+  }
+
+  delete(findOptions: object) {
+    return this.reviewRepo.delete(findOptions);
+  }
 }
