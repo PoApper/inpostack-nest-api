4152884b69706c5106f22333bd5240d4e49cbb4d
bluehorn07 <bluehornblues@gmail.com>
2021-09-18 16:24:43 +0900

    [SSO] implt auth.module with SSO

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,30 +1,27 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Get,
   Inject,
-  Param,
-  Patch,
   Post,
   Query,
   Req,
   Res,
-  UnauthorizedException,
   UseGuards,
 } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { InPoStackAuth } from './guard/InPoStackAuth.guard';
 import { AuthService } from './auth.service';
-import { Request, Response } from 'express';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { MailService } from '../mail/mail.service';
+import { ApiTags } from '@nestjs/swagger';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
+import { Public } from 'nest-keycloak-connect';
+import { parseToken } from 'nest-keycloak-connect/util';
+
 import { AccountService } from '../inpostack/account/account.service';
-import { AccountStatus, AccountType } from '../inpostack/account/account.meta';
-import { AccountCreateDto } from '../inpostack/account/account.dto';
 import { StoreService } from '../inpostack/market/store/store.service';
-import { JwtGuard } from './guard/jwt.guard';
 import { AllowAnonymous } from './decorator/anonymous.decorator';
+import { RefreshTokenDto } from './auth.dto';
 
 /**
  * This is for handle "verifyToken", "login", "logout" tasks
@@ -37,98 +34,162 @@ export class AuthController {
     private authService: AuthService,
     private readonly accountService: AccountService,
     private readonly storeService: StoreService,
-    private readonly mailService: MailService,
     @Inject(WINSTON_MODULE_PROVIDER)
     private readonly logger: Logger,
   ) {}
 
-  @Get('verifyToken')
-  @UseGuards(JwtGuard)
-  @AllowAnonymous()
-  async verifyToken(@Req() req: Request) {
-    return req.user;
+  // can be register after PoApper SSO logined
+  @Post('register')
+  async register(@Req() req) {
+    const user = req.user;
+    console.log(user);
+    const existUser = await this.accountService.findOne({
+      keycloak_id: user.sub,
+    });
+    if (existUser) {
+      throw new BadRequestException('Already Registered User');
+    }
+    const newUser = await this.accountService.save({
+      keycloak_id: user.sub,
+      email: user.email,
+      name: user.name,
+    });
+    console.log(newUser);
   }
 
-  @Post('login')
-  @UseGuards(AuthGuard('local'))
-  async login(@Req() req: Request, @Res() res: Response) {
-    try {
-      const user: any = req.user;
-      const token = this.authService.generateJwtToken(user);
-      res.setHeader('Set-Cookie', `Authentication=${token}; HttpOnly; Path=/;`);
-      this.accountService.updateLoginById(user.id);
-      this.logger.info(`Login Succeed!: uuid=${user.uuid}`);
-      return res.send(user);
-    } catch (err) {
-      this.logger.error(`Failed to login...${err}`);
+  @Get('login')
+  @Public()
+  login(
+    @Res() res,
+    @Query('redirect') redirect: string,
+    @Query('login_type') login_type: string,
+  ) {
+    // Redirect to SSO
+    let url: string;
+
+    url =
+      process.env.SSO_BASE_END +
+      '/auth?response_type=code' +
+      `&client_id=${process.env.SSO_CLIENT}` +
+      `&redirect_uri=${process.env.SSO_REDIRECT}`;
+
+    if (redirect) url += `?redirect=${redirect}`;
+    if (login_type) url += `?login_type=${login_type}`;
+
+    res.redirect(url);
+  }
+
+  @Get('callback')
+  @Public()
+  async loginCallback(
+    @Req() req,
+    @Res() res,
+    @Query('redirect') redirect: string,
+    @Query('login_type') login_type: string,
+    @Query('session_state') session_state: string,
+    @Query('code') code: string,
+  ) {
+    const jwts = await this.authService.getToken(
+      session_state,
+      code,
+      redirect,
+      login_type,
+    ); // { access_token: 'xxxx', refresh_token: 'xxxx', session_state: 'xxxx', scope: 'xxxx' }
+
+    if (login_type == 'token') {
+      // Return the JWT token in the comment_body
+      // return res.status(HttpStatus.OK).json(jwts);
+      return res.render('login', { token: JSON.stringify(jwts) });
+    } else {
+      // Default to returning cookies
+      res.cookie('Authentication', jwts.access_token, { httpOnly: true });
+      res.cookie('Refresh', jwts.refresh_token, { httpOnly: true });
+
+      // Find keycloak user exist in DB, if not redirect to InPoStack register process
+      const keycloak_user: any = parseToken(jwts.access_token);
+      console.log(keycloak_user);
+      console.log(keycloak_user.sub);
+      const existUser = await this.accountService.findOne({
+        keycloak_id: keycloak_user.sub,
+      });
+      console.log('HAHAHAHAH', existUser);
+      if (existUser) {
+        if (redirect) res.redirect(redirect);
+        else res.redirect(process.env.SSO_CALLBACK_DEFAULT);
+      } else {
+        res.redirect(`${process.env.SSO_CALLBACK_DEFAULT}/register`);
+      }
     }
+  }
+
+  // Refresh token for cookie-based login
+  @Get('refresh')
+  @Public()
+  async refreshCookie(
+    @Req() req,
+    @Res() res,
+    @Query('redirect') redirect: string,
+  ) {
+    const jwts = await this.authService.refreshToken(req.cookies['Refresh']);
+
+    if (jwts.error) {
+      // Invalid refresh token, sender should log in again
+      if (redirect) res.redirect(`/auth/login?redirect=${redirect}`);
+      else res.redirect('/auth/login');
+    } else {
+      res.cookie('Authentication', jwts.access_token, { httpOnly: true });
+
+      if (redirect) res.redirect(redirect);
+      else res.redirect(process.env.SSO_CALLBACK_DEFAULT);
+    }
+  }
+
+  // Refresh token for token-based login
+  @Post('refresh')
+  @Public()
+  async refreshToken(@Req() req, @Body() dto: RefreshTokenDto) {
+    // In case of error (e.g. refresh token expired)
+    // {
+    //     "error": "invalid_grant",
+    //     "error_description": "Invalid refresh token"
+    // }
+    //
+    // In case of success, the comment_body should contain a new access token.
+    // Unlike the cookie-based endpoint, this leaves error handling to the app.
+
+    const refreshToken = dto.refresh_token ?? req.cookies['Refresh'];
+    return await this.authService.refreshToken(refreshToken);
+  }
+
+  /**
+   * If you have a valid Access-Token, then it returns its sender information saved in DB.
+   * Otherwise, it returns `ForbiddenException` from InPoStackAuth
+   */
+
+  @Get('verifyToken')
+  @UseGuards(InPoStackAuth)
+  @AllowAnonymous()
+  verifyToken(@Req() req) {
+    const user = req.user;
+    if (user) {
+      this.accountService.updateLoginById(user.uuid);
+    }
+    return user;
   }
 
   @Get('logout')
-  @UseGuards(AuthGuard('jwt'))
-  logout(@Res() res: Response) {
-    try {
-      res.setHeader(
-        'Set-Cookie',
-        `Authentication=; HttpOnly; Path=/; Max-Age=0`,
-      );
-      this.logger.info(`Logout Succeed!`);
-      return res.sendStatus(200);
-    } catch (err) {
-      this.logger.error(`Failed to logout...${err}`);
-    }
-  }
+  @UseGuards(InPoStackAuth)
+  async logout(@Req() req, @Res() res) {
+    const user = req.user;
 
-  @Post('register')
-  @ApiOperation({
-    summary: '회원가입',
-    description: '계정 생성 & 인증 메일 발송 (admin 생성 불가)',
-  })
-  async register(
-    @Body() dto: AccountCreateDto,
-    @Query('sendMail') sendMail?: boolean,
-  ) {
-    if (dto.account_type === AccountType.admin) {
-      throw new UnauthorizedException(
-        'Not authorized to create admin account.',
-      );
-    }
+    await this.authService.logoutSso(req.cookies['Refresh']);
 
-    let newAccount;
-    try {
-      newAccount = await this.accountService.save(dto);
-      console.log(newAccount);
-    } catch (err) {
-      this.logger.error(`Failed to register...${err}`);
-    }
+    await this.authService.invalidateToken(req.cookies['Authentication']);
+    await this.authService.invalidateToken(req.cookies['Refresh']);
 
-    if (sendMail) {
-      try {
-        this.mailService.sendVerificationMail(
-          newAccount.email,
-          newAccount.uuid,
-        );
-        this.logger.info('Succeed to send email!');
-      } catch (err) {
-        this.logger.error(`Failed to send email...${err}`);
-      }
-    }
-    this.logger.info(`Succeed to register: uuid=${newAccount.uuid}`);
-    return newAccount;
-  }
-
-  @Patch('activateAccount/:uuid')
-  async activateAccount(@Param('uuid') uuid: string, @Res() res: Response) {
-    try {
-      await this.accountService.findOneOrFail({ uuid: uuid });
-      await this.accountService.update(
-        { uuid: uuid },
-        { account_status: AccountStatus.activated },
-      );
-      this.logger.info(`Succeed to active account: uuid=${uuid}`);
-      res.redirect(process.env.public_web);
-    } catch (err) {
-      this.logger.error(`Failed to activate account...${err}`);
-    }
+    res.redirect(process.env.SSO_CALLBACK_DEFAULT);
+    this.logger.info(
+      `Logout (web): uuid=${user.uuid}, origin: ${req.header('Origin')}`,
+    );
   }
 }
