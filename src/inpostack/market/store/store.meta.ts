53c91a6ecdfa674bf6b5adeba985a21d1bc1260e
bluehorn07 <bluehornblues@gmail.com>
2021-10-13 18:11:02 +0900

    update store types

--- src/inpostack/market/store/store.meta.ts
+++ src/inpostack/market/store/store.meta.ts
@@ -4,14 +4,17 @@
  */
 
 export enum StoreType {
-  korean = 'KOREAN',
-  japanese = 'JAPANESE',
-  chinese = 'CHINESE',
-  instant = 'INSTANT',
-  chicken = 'CHICKEN',
-  hamburger = 'HAMBURGER',
-  pizza = 'PIZZA',
+  korean = 'KOREAN', // 한식, 정식
+  japanese = 'JAPANESE', // 일식
+  chinese = 'CHINESE', // 중식
+  chicken = 'CHICKEN', // 치킨
+  hamburger = 'HAMBURGER', // 햄버거
+  pizza = 'PIZZA', // 피자
   snack_shop = 'SNACK_SHOP', // 분식
-  sea_food = 'SEA_FOOD',
+  sea_food = 'SEA_FOOD', // 해산물
+  dessert = 'DESSERT', // 디저트
+  korean_stew = 'KOREAN_STEW', // 찌개류. 국밥
+  western = 'WESTERN', // 양식
+  bossam = 'BOSSAM', // 고기, 보쌈
   etc = 'ETC',
 }
