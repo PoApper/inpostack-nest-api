1d0ab11612fb9d41a897331d127e84dace4c225f
bluehorn07 <bluehornblues@gmail.com>
2021-12-07 00:28:26 +0900

    rename API route and refine code

--- src/statistics/statistics.controller.ts
+++ src/statistics/statistics.controller.ts
@@ -6,31 +6,28 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger';
 @Controller('statistics')
 export class StatisticsController {
   @ApiOperation({ summary: 'Daily Register User' })
-  @Get('DRU')
+  @Get('daily_register_user')
   async dailyRegisterUser(
     @Query('start_date') start_date: Date,
     @Query('end_date') end_date: Date,
   ) {
-    return (
-      createQueryBuilder('account')
-        .select('COUNT(*) AS DRU')
-        // .addSelect('strftime("%Y-%m-%d", created_at) AS register_date') // sqlite
-        .addSelect(`DATE_FORMAT(created_at, '%Y-%m-%d') AS register_date`) // mysql
-        .where(
-          `created_at BETWEEN '${new Date(
-            start_date ?? 0,
-          ).toISOString()}' AND '${new Date(
-            end_date ?? Date.now(),
-          ).toISOString()}}'`,
-        )
-        .groupBy('register_date')
-        .orderBy('register_date')
-        .getRawMany()
-    );
+    return createQueryBuilder('account')
+      .select('COUNT(*) AS DRU')
+      .addSelect(`DATE_FORMAT(created_at, '%Y-%m-%d') AS register_date`) // mysql
+      .where(
+        `created_at BETWEEN '${new Date(
+          start_date ?? 0,
+        ).toISOString()}' AND '${new Date(
+          end_date ?? Date.now(),
+        ).toISOString()}}'`,
+      )
+      .groupBy('register_date')
+      .orderBy('register_date')
+      .getRawMany();
   }
 
   @ApiOperation({ summary: 'Daily Active User' })
-  @Get('DAU')
+  @Get('daily_active_user')
   async dailyActiveUser(
     @Query('start_date') start_date: Date,
     @Query('end_date') end_date: Date,
@@ -51,22 +48,19 @@ export class StatisticsController {
     @Query('start_date') start_date: Date,
     @Query('end_date') end_date: Date,
   ) {
-    return (
-      createQueryBuilder('store_visit')
-        .select('COUNT(*) AS TDSV')
-        // .addSelect('strftime("%Y-%m-%d", visited_at) AS visit_date') // sqlite
-        .addSelect(`DATE_FORMAT(visited_at, '%Y-%m-%d') AS visit_date`) // mysql
-        .where(
-          `visited_at BETWEEN '${new Date(
-            start_date ?? 0,
-          ).toISOString()}' AND '${new Date(
-            end_date ?? Date.now(),
-          ).toISOString()}}'`,
-        )
-        .groupBy('visit_date')
-        .orderBy('visit_date')
-        .getRawMany()
-    );
+    return createQueryBuilder('store_visit')
+      .select('COUNT(*) AS TDSV')
+      .addSelect(`DATE_FORMAT(visited_at, '%Y-%m-%d') AS visit_date`) // mysql
+      .where(
+        `visited_at BETWEEN '${new Date(
+          start_date ?? 0,
+        ).toISOString()}' AND '${new Date(
+          end_date ?? Date.now(),
+        ).toISOString()}}'`,
+      )
+      .groupBy('visit_date')
+      .orderBy('visit_date')
+      .getRawMany();
   }
 
   @ApiOperation({ summary: 'Daily Store Visit' })
@@ -75,24 +69,21 @@ export class StatisticsController {
     @Query('start_date') start_date: Date,
     @Query('end_date') end_date: Date,
   ) {
-    return (
-      createQueryBuilder('store_visit')
-        .select('COUNT(*) AS DSV')
-        .addSelect('store_uuid')
-        // .addSelect('strftime("%Y-%m-%d", visited_at) AS visit_date') // sqlite
-        .addSelect(`DATE_FORMAT(visited_at, '%Y-%m-%d') AS visit_date`) // mysql
-        .where(
-          `visited_at BETWEEN '${new Date(
-            start_date ?? 0,
-          ).toISOString()}' AND '${new Date(
-            end_date ?? Date.now(),
-          ).toISOString()}}'`,
-        )
-        .groupBy('visit_date')
-        .addGroupBy('store_uuid')
-        .orderBy('visit_date')
-        .addOrderBy('store_uuid')
-        .getRawMany()
-    );
+    return createQueryBuilder('store_visit')
+      .select('COUNT(*) AS DSV')
+      .addSelect('store_uuid')
+      .addSelect(`DATE_FORMAT(visited_at, '%Y-%m-%d') AS visit_date`) // mysql
+      .where(
+        `visited_at BETWEEN '${new Date(
+          start_date ?? 0,
+        ).toISOString()}' AND '${new Date(
+          end_date ?? Date.now(),
+        ).toISOString()}}'`,
+      )
+      .groupBy('visit_date')
+      .addGroupBy('store_uuid')
+      .orderBy('visit_date')
+      .addOrderBy('store_uuid')
+      .getRawMany();
   }
 }
