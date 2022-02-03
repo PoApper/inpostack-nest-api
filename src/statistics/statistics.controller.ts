bcae15b62952994e3e80b75c3d8e9f413e82a666
jjeongone <jjeongone@gmail.com>
2022-02-03 21:29:41 +0900

    [fix] refactoring query code

--- src/statistics/statistics.controller.ts
+++ src/statistics/statistics.controller.ts
@@ -47,29 +47,4 @@ export class StatisticsController {
   ) {
     return this.statisticsService.storeVisitTimeQuery(startDate, endDate, storeUuid)
   }
-
-  @ApiOperation({summary: 'Store Visit Time'})
-  @Get('store_visit_time')
-  async storeVisitTime(@Query('start_date') startDate: Date,
-                       @Query('end_date') endDate: Date,
-                       @Query('store_uuid') storeUuid: string
-  ) {
-    const startDateUtc = new Date(startDate);
-    const endDateUtc = new Date(endDate);
-    startDateUtc.setHours(startDateUtc.getHours()-9);
-    endDateUtc.setHours(endDateUtc.getHours()-9);
-    return (
-      createQueryBuilder('store_visit_event')
-        .select([
-          `DATE_FORMAT(CONVERT_TZ(visited_at, '+00:00','+09:00'), '%h:%i') AS visit_hour`,
-          `visited_at`,
-          'COUNT(*) AS visit_count'
-        ])
-        .where(`visited_at BETWEEN '${startDateUtc.toISOString()}' AND '${endDateUtc.toISOString()}'`)
-        .andWhere(`store_uuid = '${storeUuid}'`)
-        .groupBy('visit_hour')
-        .orderBy('visited_at')
-        .getRawMany()
-    );
-  }
 }
