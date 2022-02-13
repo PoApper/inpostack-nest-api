ae63fc94cbb7645e1b3d2890ea9716f06b9744c1
bluehorn07 <bluehornblues@gmail.com>
2022-02-14 01:23:47 +0900

    polish and add TODOs

--- src/utils/findDistance.ts
+++ src/utils/findDistance.ts
@@ -1,28 +1,44 @@
-import axios from "axios";
+import axios from 'axios';
 
-async function findDistance(storeAddress: string){
-    const addressInfo = await axios.get( "https://dapi.kakao.com/v2/local/search/address.json", {
-        headers: { 
-            'Accept': 'application/json',
-            'Authorization': process.env.KAKAO_KEY
-        },
-        params: {
-            query: storeAddress,
-        }
-    })
-    const coordinateValue = addressInfo.data.documents[0].address.x + "," +  addressInfo.data.documents[0].address.y
+// TODO: write function description here
+// TODO: write storeAddress example, zipcode? or Korean address?
+/**
+ *
+ * @param storeAddress
+ */
 
-    const distance = await axios.get( 'https://apis-navi.kakaomobility.com/v1/directions', {
-        headers: { 
-            'Accept': 'application/json',
-            'Authorization': process.env.KAKAO_KEY
-        },
-        params: {
-            origin: "129.322359, 36.015702", // 지곡회관 좌표
-            destination: coordinateValue
-        }
-    })
-    return distance.data.routes[0].summary.distance
+async function findDistance(storeAddress: string) {
+  const addressInfo = await axios.get(
+    'https://dapi.kakao.com/v2/local/search/address.json',
+    {
+      headers: {
+        Accept: 'application/json',
+        Authorization: process.env.KAKAO_KEY,
+      },
+      params: {
+        query: storeAddress,
+      },
+    },
+  );
+  const coordinateValue: string =
+    addressInfo.data.documents[0].address.x +
+    ',' +
+    addressInfo.data.documents[0].address.y;
+
+  const distance = await axios.get(
+    'https://apis-navi.kakaomobility.com/v1/directions',
+    {
+      headers: {
+        Accept: 'application/json',
+        Authorization: process.env.KAKAO_KEY,
+      },
+      params: {
+        origin: '129.322359, 36.015702', // 지곡회관 좌표
+        destination: coordinateValue,
+      },
+    },
+  );
+  return distance.data.routes[0].summary.distance;
 }
 
 export default findDistance;
