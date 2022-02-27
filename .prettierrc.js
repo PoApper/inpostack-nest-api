d139aca3346f3f59536648c39b0ec7ccb6929049
bluehorn07 <bluehornblues@gmail.com>
2022-02-27 11:58:03 +0900

    refactor ESLint and prettier config file

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+/**
+ * <Prettier Guide>
+ * [WebStorm IDE]
+ * Settings -> Plugins -> Install 'Prettier' -> Apply
+ * Settings -> Language & Frameworks -> Prettier -> Check 'on Reformat Code' action
+ *
+ * If you change some prettier rules, then restart your IDE!!
+ */
+
+module.exports = {
+  singleQuote: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  endOfLine: 'lf',
+};
