04e9b9aefe338d20b31c06e81c9ea054bd3ab05d
bluehorn07 <bluehornblues@gmail.com>
2022-03-01 22:43:22 +0900

    fix module dependency

--- src/inpostack/market/category/category.module.ts
+++ src/inpostack/market/category/category.module.ts
@@ -4,9 +4,10 @@ import { CategoryService } from './category.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './category.entity';
 import { StoreModule } from '../store/store.module';
+import { AuthModule } from '../../../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category]), StoreModule],
+  imports: [TypeOrmModule.forFeature([Category]), StoreModule, AuthModule],
   controllers: [CategoryController],
   providers: [CategoryService],
   exports: [CategoryService],
