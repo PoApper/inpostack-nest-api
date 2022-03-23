0d8bfcf344c64c77537c69cb5e0c034cbaf075d0
bluehorn07 <bluehornblues@gmail.com>
2022-03-24 00:34:21 +0900

    update cors rule

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://localhost:3001',
       'http://localhost:3002',
-      'https://inpo.poapper.club',
-      'https://admin.inpo.poapper.club',
-      'https://store.inpo.poapper.club',
+      /^(https:\/\/([^\.]*\.)?inpostack\.poapper\.club)$/i,
     ],
     credentials: true,
   });
